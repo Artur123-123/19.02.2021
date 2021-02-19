f98075e93b6f0f9a3158f48c606f9bb6ee4c8f55
Artur-123-123 <arturolejnik462@gmail.com>
2021-02-19 15:49:05 +0100

    19.02.2021

--- main.js
+++ main.js
@@ -52,11 +52,40 @@ console.log(tablicaImion)
 
 const names = ['Marzena', 'Jaro', 'Alicja', 'Magdalena', 'Robert']
 
-const i = 1
+// const i = 1
 const numbersTillHundred = new Array()
 
-for (let i = 1; i < 101; i++) {
-    numbersTillHundred.push(i + 1)
-    numbersTillHundred.pop()
+for (let i = 100; i > 0; i--) {
+    numbersTillHundred.push(i)
+        // numbersTillHundred.reverse()
+        // numbersTillHundred.pop()
 }
 console.log(numbersTillHundred)
+
+const multiply1 = numbersTillHundred.map(item => item * 3)
+console.log(multiply1)
+
+const higherThanFive = numbersTillHundred.every(item => item > 5)
+console.log(higherThanFive)
+
+const higherThanFiveSome = numbersTillHundred.some(item => item > 5)
+console.log(higherThanFiveSome)
+
+
+const xd2 = numbersTillHundred.filter(item => {
+    if (item > 39 && item < 61) {
+        return item
+    }
+})
+xd2.reverse()
+console.log(xd2)
+
+const fourtysix = numbersTillHundred.indexOf(46, 45)
+console.log(fourtysix)
+
+// const sixty = new Array()
+// for (let i = 40; i < 61; i++) {
+//     sixty.push(i)
+// }
+
+// console.log(sixty)
